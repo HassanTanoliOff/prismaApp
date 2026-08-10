987603beb007e60c83bb1f1250d010d997b28728
HassanTanoliOff <hassantanolioff@gmail.com>
2026-08-10 18:35:02 +0500

    update category

--- src/repos/category.repo.ts
+++ src/repos/category.repo.ts
@@ -1,8 +1,12 @@
 import prisma from "../lib/prisma.js";
 
 export const addCategoryRepo = async (categoryName: string) => {
-  return await prisma.category.create({
-    data: {
+  return await prisma.category.upsert({
+    where: {
+      category_name: categoryName,
+    },
+    update: {},
+    create: {
       category_name: categoryName,
     },
   });
